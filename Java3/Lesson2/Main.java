72029e64f238782a67b409baa336de597f8f37d1
Andrey <sozykin@yandex.ru>
2020-10-01 16:13:46 +0300

    J3Lesson2

--- Java3/Lesson2/Main.java
+++ Java3/Lesson2/Main.java
@@ -1,7 +1,7 @@
 import javax.jws.soap.SOAPBinding;
 
 public class Main {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         try {
             DbProcessor conn = new DbProcessor();
             conn.setQueryResult("SELECT * FROM _user");
