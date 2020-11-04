eb4241f6000fb0d9bc3c08a73b5cdd23ce8b8c76
Andrey <sozykin@yandex.ru>
2020-11-04 13:47:23 +0300

    dbLesson2

---
+++ db/Lesson2/fix_idx.sql
@@ -0,0 +1,20 @@
+ALTER TABLE `geodata`.`_countries` 
+ADD INDEX `idx_title` (`title` ASC);
+INSERT INTO `geodata`.`_countries` (`title`) VALUES ('NOT_DEFINED');
+
+ALTER TABLE `geodata`.`_regions` 
+CHANGE COLUMN `country_id` `country_id` INT(11) NOT NULL DEFAULT 236 ,
+ADD INDEX `title` (`title` ASC),
+ADD INDEX `fk_country_id_idx` (`country_id` ASC);
+;
+ALTER TABLE `geodata`.`_regions` 
+ADD CONSTRAINT `fk_country_id`
+  FOREIGN KEY (`country_id`)
+  REFERENCES `geodata`.`_countries` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+
+ALTER TABLE `geodata`.`_cities` 
+ADD INDEX `idx_title` (`title` ASC);
+;
+
